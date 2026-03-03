948e58e0b77687a64c7459415550fef80d7d8e3e
rudybrrr <agnerudhresh01@gmail.com>
2026-03-03 18:43:23 +0800

    Added a stats page with a calendar (will add other insights etc. soon too) + styling

--- cycling_app/views.py
+++ cycling_app/views.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from datetime import datetime
 from . import app
 from .database import insert_ride, get_rides, get_stats
+import calendar
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
@@ -9,8 +10,11 @@ def home():
         distance = request.form.get('distance')
         duration = request.form.get('duration')
         date = request.form.get('date')
-        insert_ride(distance, duration, date)
-        flash(f"Ride logged! Distance: {distance}km, Duration: {duration}mins, Date: {date}")
+        if float(distance) > 0 and float(duration) > 0: 
+            insert_ride(distance, duration, date)
+            flash(f"Ride logged! Distance: {distance}km, Duration: {duration}mins, Date: {date}")
+        else: 
+            flash("Invalid values. Distance and duration must be greater than 0.")
         return redirect(url_for('home'))
     else:
         stats = get_stats()
@@ -20,3 +24,21 @@ def home():
             stats=stats, 
             rides=rides
         )
+
+@app.route("/stats/")
+def stats(): 
+    rides = get_rides()
+    ride_dates = {ride[3] for ride in rides}
+    today = datetime.now()
+    month_name = today.strftime("%B")
+    cal = calendar.monthcalendar(today.year, today.month)
+    print(ride_dates)
+    return render_template(
+        "stats.html", 
+        rides=rides, 
+        ride_dates=ride_dates,
+        month = today.month,
+        year = today.year, 
+        month_name=month_name,
+        cal=cal
+    )
